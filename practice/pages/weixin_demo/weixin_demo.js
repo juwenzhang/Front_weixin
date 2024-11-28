4083bbb49d238d92012a8ff2daffe938ef520b52
juwenzhang <3137536916@qq.com>
2024-11-28 10:22:52 +0800

    feat: four and Update README.md

---
+++ practice/pages/weixin_demo/weixin_demo.js
@@ -0,0 +1,27 @@
+// pages/weixin_demo/weixin_demo.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    message: "巴黎奥运会",
+    id: "demo",
+    is_login: true,
+    objectArray: [
+      {id: 5, unique: 'unique_5'},
+      {id: 4, unique: 'unique_4'},
+      {id: 3, unique: 'unique_3'},
+      {id: 2, unique: 'unique_2'},
+      {id: 1, unique: 'unique_1'},
+      {id: 0, unique: 'unique_0'},
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+
+  }
+})    
